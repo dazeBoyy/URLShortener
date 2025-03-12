5de2fda055b0d14de8a7abdd5189ba76e9a1e0aa
dazeBoyy <pavelnosorog@mail.ru>
2025-03-12 22:02:57 +0300

    fixed

--- internal/app/run.go
+++ internal/app/run.go
@@ -12,18 +12,28 @@ import (
 
 func Run() error {
 	var repo repository.Repository
+	// Получаем значение переменной окружения STORAGE для выбора типа хранилища.
+	storageType := os.Getenv("STORAGE")
 
-	dbURL := os.Getenv("DATABASE_URL")
-	if dbURL != "" {
+	if storageType == "postgres" {
+		// Если STORAGE = postgres, используем PostgreSQL
+		dbURL := os.Getenv("DATABASE_URL")
+		if dbURL == "" {
+			return fmt.Errorf("DATABASE_URL is required when using PostgreSQL")
+		}
 		fmt.Println("Using PostgreSQL storage")
 		ps, err := repository.NewPostgresStorage(dbURL)
 		if err != nil {
 			return fmt.Errorf("failed to connect to PostgreSQL: %w", err)
 		}
 		repo = ps
-	} else {
+	} else if storageType == "memory" {
+		// Если STORAGE != postgres или не задано, используем in-memory storage
 		fmt.Println("Using in-memory storage")
 		repo = repository.NewInMemoryStorage()
+	} else {
+		// Если STORAGE не задан или задан некорректно, возвращаем ошибку
+		return fmt.Errorf("invalid storage type: %s, use 'postgres' or 'memory'", storageType)
 	}
 
 	// Создаем экземпляр сервиса для сокращения ссылок, но присваиваем переменной с типом интерфейса для гибкости.
